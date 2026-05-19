059936c3b531c02c27a6de3868b04c9a6f85d6da
Shashank S <shashanksmithamanya@gmail.com>
2026-05-19 14:57:48 +0530

    Add reusable centralized logging utility

---
+++ utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+def setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(level)
+
+    if not logger.handlers:
+
+        ch = logging.StreamHandler()
+
+        ch.setLevel(level)
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+
+        ch.setFormatter(formatter)
+
+        logger.addHandler(ch)
+
+    return logger
